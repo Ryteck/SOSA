18cd83e2d42e3da61dffe2035dd1321e2b61bff4
Ryan Gomes Martins <ryteckgm@gmail.com>
2024-01-26 13:21:28 -0300

    Start project

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: 'next-env.d.ts',
+      rules: { '@typescript-eslint/triple-slash-reference': 'off' },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // React
+    'react/react-in-jsx-scope': 'off',
+
+    // Prettier
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react'
+
+const Page: FC = () => (
+  <main className="w-screen h-screen flex items-center justify-center">
+    Hello, World!
+  </main>
+)
+
+export default Page
