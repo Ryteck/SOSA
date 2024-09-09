2688a706a52600dc4d7faa0d6040eed61cf2bf60
Ryan Gomes Martins <ryteckgm@gmail.com>
2024-09-09 14:14:50 -0300

    Add Scroll into session config

--- src/components/configure-session.tsx
+++ src/components/configure-session.tsx
@@ -26,6 +26,7 @@ import { GearIcon, OpenInNewWindowIcon } from "@radix-ui/react-icons";
 import { type FC, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useSWRConfig } from "swr";
+import {ScrollArea} from "@/components/ui/scroll-area";
 
 interface Props {
 	sessionId: string;
@@ -66,87 +67,90 @@ export const ConfigureSession: FC<Props> = ({ sessionId }) => {
 			</TooltipProvider>
 
 			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Edit session</DialogTitle>
-					<DialogDescription>Available session locations</DialogDescription>
-				</DialogHeader>
 
-				<div className="flex flex-col gap-2">
-					{locations?.map((local) => {
-						const checkId = `local:${local.id}`;
+					<DialogHeader>
+						<DialogTitle>Edit session</DialogTitle>
+						<DialogDescription>Available session locations</DialogDescription>
+					</DialogHeader>
 
-						return (
-							<div key={local.id} className="flex items-center gap-2">
-								<Checkbox
-									id={checkId}
-									checked={actives.includes(local.id)}
-									onCheckedChange={(checked) => {
-										setActives((state) => {
-											if (checked) return [...state, local.id];
-											return state.filter((arg) => arg !== local.id);
-										});
-									}}
-								/>
-								<Label
-									className="flex items-center gap-2 text-lg"
-									htmlFor={checkId}
-								>
-									<p>
-										{local.campus.name} - {local.name}
-									</p>
-									<p className="text-xs opacity-40">{local.details}</p>
-								</Label>
-							</div>
-						);
-					})}
-				</div>
+				<ScrollArea className="h-96 pr-8">
+					<div className="flex flex-col gap-2">
+						{locations?.map((local) => {
+							const checkId = `local:${local.id}`;
 
-				<DialogFooter className="mt-8">
-					<Button
-						variant="destructive"
-						onClick={() => {
-							toast
-								.promise(
-									destroySessionById(sessionId).then(
-										async () => await mutate("api/sessions"),
-									),
-									{
-										loading: "closing, please wait...",
-										success: "closed",
+							return (
+								<div key={local.id} className="flex items-center gap-2">
+									<Checkbox
+										id={checkId}
+										checked={actives.includes(local.id)}
+										onCheckedChange={(checked) => {
+											setActives((state) => {
+												if (checked) return [...state, local.id];
+												return state.filter((arg) => arg !== local.id);
+											});
+										}}
+									/>
+									<Label
+										className="flex items-center gap-2 text-lg"
+										htmlFor={checkId}
+									>
+										<p>
+											{local.campus.name} - {local.name}
+										</p>
+										<p className="text-xs opacity-40">{local.details}</p>
+									</Label>
+								</div>
+							);
+						})}
+					</div>
+				</ScrollArea>
+
+					<DialogFooter className="mt-8">
+						<Button
+							variant="destructive"
+							onClick={() => {
+								toast
+									.promise(
+										destroySessionById(sessionId).then(
+											async () => await mutate("api/sessions"),
+										),
+										{
+											loading: "closing, please wait...",
+											success: "closed",
+											error: "unknown error",
+										},
+									)
+									.catch(console.error);
+							}}
+						>
+							Close session
+						</Button>
+
+						<Button
+							variant="secondary"
+							onClick={() => {
+								window.open(`/sessions/${sessionId}`, "_blank");
+							}}
+						>
+							<OpenInNewWindowIcon className="mr-2 h-4 w-4" />
+							share session
+						</Button>
+
+						<Button
+							variant="default"
+							onClick={() => {
+								toast
+									.promise(setLocationsById(sessionId, actives), {
+										loading: "saving, please wait...",
+										success: "saved",
 										error: "unknown error",
-									},
-								)
-								.catch(console.error);
-						}}
-					>
-						Close session
-					</Button>
-
-					<Button
-						variant="secondary"
-						onClick={() => {
-							window.open(`/sessions/${sessionId}`, "_blank");
-						}}
-					>
-						<OpenInNewWindowIcon className="mr-2 h-4 w-4" />
-						share session
-					</Button>
-
-					<Button
-						variant="default"
-						onClick={() => {
-							toast
-								.promise(setLocationsById(sessionId, actives), {
-									loading: "saving, please wait...",
-									success: "saved",
-									error: "unknown error",
-								})
-								.catch(console.error);
-						}}
-					>
-						Save changes
-					</Button>
-				</DialogFooter>
+									})
+									.catch(console.error);
+							}}
+						>
+							Save changes
+						</Button>
+					</DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
